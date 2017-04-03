34044f5c42070bbcb31a466ceb707ec4ecf716ed
Sujil Anto <sujilnt@gmail.com>
2017-04-03 22:23:15 +0530

    adding some more changes

--- src/main.js
+++ src/main.js
@@ -15,30 +15,55 @@ var columnDefs = [
     {headerName: "reservationid", field: "reservationid", width: 350, enableRowGroup: true, enablePivot: true},
     {headerName: "confnum", field: "confnum", width: 90, enableValue: true},
     {headerName: "creationdt", field: "creationdt", width: 250, enableRowGroup: true, enablePivot: true},
-    {headerName: "arrivaldt", field: "arrivaldt", width: 90, enableRowGroup: true, enablePivot: true,filter: 'set', filterParams:{selectAllOnMiniFilter:true}},
+    {headerName: "arrivaldt", field: "arrivaldt", width: 90, enableRowGroup: true, enablePivot: true, filter: 'date',
+        filterParams:{
+
+            // provide comparator function
+            comparator: function (filterLocalDateAtMidnight, cellValue) {
+
+                // In the example application, dates are stored as dd/mm/yyyy
+                // We create a Date object for comparison against the filter date
+                var dateParts  = cellValue.split("/");
+                var day = Number(dateParts[1]);
+                var month = Number(dateParts[0]) - 1;
+                var year = Number('20'+dateParts[2]);
+                var cellDate = new Date(day, month, year);
+
+                // Now that both parameters are Date objects, we can compare
+                if (cellDate < filterLocalDateAtMidnight) {
+                    return -1;
+                } else if (cellDate > filterLocalDateAtMidnight) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        }
+    },
     {headerName: "departuredt", field: "departuredt", width: 110, enableRowGroup: true, enablePivot: true},
-    {headerName: "nights", field: "nights", width: 110, enableRowGroup: true, enablePivot: true,filter: 'set', filterParams:{selectAllOnMiniFilter:true}},
-    {headerName: "ratecode", field: "ratecode", width: 100, enableValue: true,enableRowGroup: true, aggFunc: 'sum'},
-    {headerName: "roomnum", field: "roomnum", width: 100,enableRowGroup: true, enablePivot: true},
-    {headerName: "roomtype", field: "roomtype", width: 100, enableValue: true, enablePivot: true},
-    {headerName: "reservationtype", field: "reservationtype", width: 150, enableValue: true,enableRowGroup: true},
-    {headerName: "marketcd", field: "marketcd", width: 100, enableValue: true,enableRowGroup: true},
-    {headerName: "marketname", field: "marketname", width: 100,enableRowGroup: true, enableValue: true},
-    {headerName: "sourcecd", field: "sourcecd", width: 100, enableRowGroup: true,enableValue: true},
-    {headerName: "sourcename", field: "sourcename", width: 100,enableRowGroup: true, enableValue: true},
-    {headerName: "guestid", field: "guestid", width: 100, enableRowGroup: true, enableValue: true},
-    {headerName: "firstname", field: "firstname", width: 200,enableRowGroup: true, enableValue: true},
-    {headerName: "lastname", field: "lastname", width: 200, enableRowGroup: true, enableValue: true},
-    {headerName: "email", field: "email", width: 200, enableRowGroup: true, enableValue: true},
-    {headerName: "staydate", field: "staydate", width: 100,enableRowGroup: true,  enableValue: true},
-    {headerName: "rate", field: "rate", width: 100,enableRowGroup: true, enableValue: true},
-    {headerName: "hotelsoftsegment", field: "hotelsoftsegment", width: 100,enableRowGroup: true, enableValue: true}
+    {headerName: "nights", field: "nights", width: 110, enableRowGroup: true, enablePivot: true},
+    {headerName: "ratecode", field: "ratecode", width: 100, enableValue: true, aggFunc: 'sum'},
+    {headerName: "roomnum", field: "roomnum", width: 100, enableValue: true},
+    {headerName: "roomtype", field: "roomtype", width: 100, enableValue: true},
+    {headerName: "reservationtype", field: "reservationtype", width: 150, enableValue: true},
+    {headerName: "marketcd", field: "marketcd", width: 100, enableValue: true},
+    {headerName: "marketname", field: "marketname", width: 100, enableValue: true},
+    {headerName: "sourcecd", field: "sourcecd", width: 100, enableValue: true},
+    {headerName: "sourcename", field: "sourcename", width: 100, enableValue: true},
+    {headerName: "guestid", field: "guestid", width: 100, enableValue: true},
+    {headerName: "firstname", field: "firstname", width: 200, enableValue: true},
+    {headerName: "lastname", field: "lastname", width: 200, enableValue: true},
+    {headerName: "email", field: "email", width: 200, enableValue: true},
+    {headerName: "staydate", field: "staydate", width: 100, enableValue: true},
+    {headerName: "rate", field: "rate", width: 100, enableValue: true},
+    {headerName: "hotelsoftsegment", field: "hotelsoftsegment", width: 100, enableValue: true}
 ];
 
 var gridOptions = {
     // set rowData to null or undefined to show loading panel by default
     showToolPanel: true,
     enableColResize: true,
+    enableFilter: true,
     columnDefs: columnDefs
 };
 
@@ -50,7 +75,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // do http request to get our sample data - not using any framework to keep the example self contained.
     // you will probably use a framework like JQuery, Angular or something else to do your HTTP calls.
 
-            gridOptions.api.setRowData(data);
+    gridOptions.api.setRowData(data);
 
 });
 
